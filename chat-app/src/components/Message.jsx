58617fefd811b3dd9c161b347fbc288d57de327a
kevinmbuicode <kevinmushan@gmail.com>
2022-09-22 16:53:27 +0300

    Styled the incoming messages

--- chat-app/src/components/Message.jsx
+++ chat-app/src/components/Message.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/9832430/pexels-photo-9832430.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        {/* <img
+          src="https://images.pexels.com/photos/9832430/pexels-photo-9832430.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        /> */}
+      </div>
+    </div>
+  );
+};
 
 export default Message;
