3e73b219e606b623d751846c8c4583c1d382e2c5
kevinmbuicode <kevinmushan@gmail.com>
2022-09-22 16:16:29 +0300

    Design achieved

--- chat-app/src/components/Chat.jsx
+++ chat-app/src/components/Chat.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import AddIcon from '../images/addImage.png'
+import Messages from './Messages'
+import Input from './Input'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className='chatInfo'>
+        <span>Jane</span>
+        <div className='chatIcons'>
+          <img src={AddIcon} alt=""/>
+          <img src={AddIcon} alt=""/>
+          <img src={AddIcon} alt=""/>
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
   )
 }
 
--- chat-app/src/components/Input.jsx
+++ chat-app/src/components/Input.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import inputIcons from '../images/addImage.png';
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className="input">
+      <input type="text" placeholder="Type something..."/>
+      <div className="send">
+        <img src={inputIcons} alt="" height="24px"/>
+        <input type="file" style={{ display: "none"}} id="file"/>
+        <label htmlFor="file">
+          <img src={inputIcons} alt="" height="24px"/>
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
 
