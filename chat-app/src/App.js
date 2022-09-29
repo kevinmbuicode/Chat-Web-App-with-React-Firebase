bbfd1374db47157fd339cebe8eeb123862d618c9
kevinmbuicode <kevinmushan@gmail.com>
2022-09-29 20:25:53 +0300

    Added userContext, firebase realtime & clean up

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import './style.scss';
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser)
+
   return (
     <BrowserRouter>
       <Routes>
